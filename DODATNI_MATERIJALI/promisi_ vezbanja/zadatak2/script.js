e07548e7f22a52d9a44d0505b19ffeeb0f57f582
MilaMi94 <mila.bogdanovic94@gmail.com>
2023-01-27 14:52:21 +0100

    promise vezbanja

--- DODATNI_MATERIJALI/promisi_ vezbanja/zadatak2/script.js
+++ DODATNI_MATERIJALI/promisi_ vezbanja/zadatak2/script.js
@@ -1,2 +1,12 @@
-let form = document.getElementById('form');
-let textInput = document.getElementById('nazivPr');
+let form = document.getElementById("form");
+let textInput = document.getElementById("nazivPr");
+let inputCenaPrva = document.getElementById("cena1");
+let inputCenaDruga = document.getElementById("cena2");
+let btnSubmit = document.querySelector('input[type="submit"]');
+
+
+//Odrediti sve proizvode koji su na stanju.
+
+// preko promisa
+
+let  
